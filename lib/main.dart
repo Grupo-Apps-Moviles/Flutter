603c13c210169240994b8772e58002e0439c8f97
Néstor Velarde Gonzales <u20211c221@upc.edu.pe>
2026-06-06 12:39:42 -0500

    feat: Refactor main function and application routing for improved initialization and structure

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:waypass_app/core/di/dependency_injection.dart';
 import 'package:waypass_app/features/auth/presentation/login_page.dart';
 import 'package:waypass_app/features/auth/presentation/login_view_model.dart';
+import 'package:waypass_app/features/main/presentation/main_page.dart';
 
-void main() {
-  setup();
+void main() async {
+  // 1. Obligatorio al usar asincronismo antes de runApp (por SharedPreferences)
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // 2. Esperamos a que todas las dependencias se inyecten correctamente
+  await setup();
+  
+  // 3. Arrancamos la aplicación
   runApp(const MainApp());
 }
 
@@ -15,10 +22,27 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BlocProvider(
-        create: (context) => getIt<LoginViewModel>(),
-        child: LoginPage(),
+      title: 'WayPass',
+      debugShowCheckedModeBanner: false, // Oculta la etiqueta de "DEBUG"
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple, // Equivalente a tus colores Purple del proyecto original
+          brightness: Brightness.light,
+        ),
+        // Tipografía por defecto si deseas aplicarla globalmente luego
+        // fontFamily: 'Poppins', 
       ),
+      // Definimos el login como pantalla inicial
+      initialRoute: '/login',
+      // Mapeo de rutas de la aplicación
+      routes: {
+        '/login': (context) => BlocProvider(
+              create: (context) => getIt<LoginViewModel>(),
+              child: LoginPage(),
+            ),
+        '/main': (context) => const MainPage(),
+      },
     );
   }
 }
