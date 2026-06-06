10aeee73075ab007a79f7154e2fcb4c0dfeb0ec5
Néstor Velarde Gonzales <u20211c221@upc.edu.pe>
2026-06-06 11:33:49 -0500

    feat: Enhance dependency injection setup to include TokenManager for improved authentication handling

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,19 +1,31 @@
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:waypass_app/features/auth/data/auth_repository_impl.dart';
 import 'package:waypass_app/features/auth/data/auth_service.dart';
+import 'package:waypass_app/features/auth/data/token_manager.dart';
 import 'package:waypass_app/features/auth/domain/auth_repository.dart';
 import 'package:waypass_app/features/auth/presentation/login_view_model.dart';
 
 final getIt = GetIt.instance;
 
-void setup() {
+Future<void> setup() async {
+  // 1. Inicializamos SharedPreferences y TokenManager
+  final sharedPreferences = await SharedPreferences.getInstance();
+  getIt.registerLazySingleton<TokenManager>(() => TokenManager(sharedPreferences));
+
+  // 2. Data Sources
   getIt.registerLazySingleton<AuthService>(() => AuthService());
 
+  // 3. Repositorios
   getIt.registerLazySingleton<AuthRepository>(
     () => AuthRepositoryImpl(service: getIt<AuthService>()),
   );
 
+  // 4. ViewModels (Cubits)
   getIt.registerFactory(
-    () => LoginViewModel(repository: getIt<AuthRepository>()),
+    () => LoginViewModel(
+      repository: getIt<AuthRepository>(),
+      tokenManager: getIt<TokenManager>(),
+    ),
   );
 }
