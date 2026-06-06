ac050c08e4a7bfc0a981a313021eb722db8d5cd0
Néstor Velarde Gonzales <u20211c221@upc.edu.pe>
2026-06-06 10:56:01 -0500

    feat: Add ProfilePage and ReservationPage as StatelessWidgets with placeholder content

---
+++ lib/features/reservation/presentation/reservation_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class ReservationPage extends StatelessWidget {
+  const ReservationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
