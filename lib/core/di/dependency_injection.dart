3309a37dcac354488e76b853b1966bd51e3cbab8
Néstor Velarde Gonzales <u20211c221@upc.edu.pe>
2026-06-06 14:53:31 -0500

    feat: Register ProfileRepository and ProfileViewModel in dependency injection setup

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -5,27 +5,37 @@ import 'package:waypass_app/features/auth/data/auth_service.dart';
 import 'package:waypass_app/features/auth/data/token_manager.dart';
 import 'package:waypass_app/features/auth/domain/auth_repository.dart';
 import 'package:waypass_app/features/auth/presentation/login_view_model.dart';
+import 'package:waypass_app/features/profile/data/profile_repository_impl.dart';
+import 'package:waypass_app/features/profile/domain/profile_repository.dart';
+import 'package:waypass_app/features/profile/presentation/profile_view_model.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> setup() async {
-  // 1. Inicializamos SharedPreferences y TokenManager
   final sharedPreferences = await SharedPreferences.getInstance();
   getIt.registerLazySingleton<TokenManager>(() => TokenManager(sharedPreferences));
 
-  // 2. Data Sources
+  // Data Sources
   getIt.registerLazySingleton<AuthService>(() => AuthService());
 
-  // 3. Repositorios
+  // Repositorios
   getIt.registerLazySingleton<AuthRepository>(
     () => AuthRepositoryImpl(service: getIt<AuthService>()),
   );
+  // REGISTRO NUEVO DEL REPOSITORIO DE PROFILE
+  getIt.registerLazySingleton<ProfileRepository>(
+    () => ProfileRepositoryImpl(tokenManager: getIt<TokenManager>()),
+  );
 
-  // 4. ViewModels (Cubits)
+  // ViewModels (Cubits)
   getIt.registerFactory(
     () => LoginViewModel(
       repository: getIt<AuthRepository>(),
       tokenManager: getIt<TokenManager>(),
     ),
+  );
+  // REGISTRO NUEVO DEL VIEWMODEL DE PROFILE
+  getIt.registerFactory(
+    () => ProfileViewModel(repository: getIt<ProfileRepository>()),
   );
 }
