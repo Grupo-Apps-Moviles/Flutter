2f6b242367e214e92a634b64d33f0b11d5537121
Adrian QC <aqc1019@gmail.com>
2026-06-16 23:53:44 -0500

    feat(reservation): add ReservationRepositoryImpl

---
+++ lib/features/reservation/data/reservation_repository_impl.dart
@@ -0,0 +1,20 @@
+import '../data/create_reservation_request.dart';
+import '../data/reservation_dto.dart';
+import '../data/reservation_service.dart';
+import '../domain/reservation_repository.dart';
+
+class ReservationRepositoryImpl implements ReservationRepository {
+  final ReservationService service;
+
+  ReservationRepositoryImpl({required this.service});
+
+  @override
+  Future<void> createReservation(CreateReservationRequest request) {
+    return service.createReservation(request);
+  }
+
+  @override
+  Future<List<ReservationDto>> getUserReservations(int userId) {
+    return service.getUserReservations(userId);
+  }
+}
